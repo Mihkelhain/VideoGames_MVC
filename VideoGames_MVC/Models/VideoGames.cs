5eb7f56d98dfd17c2a277cea8d1bbb2e27e01bda
Mihkel Hain <Mihkelhain7@gmail.com>
2023-01-25 23:30:36 +0200

    controller methods and views

--- VideoGames_MVC/Models/VideoGames.cs
+++ VideoGames_MVC/Models/VideoGames.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VideoGames_MVC.Models
 {
     public class VideoGames
     {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int AgeRating { get; set; }
-            [DataType(DataType.Date)]
-            public DateTime ReleaseDate { get; set; }
-            public string Genre { get; set; }
-            public decimal Price { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Age Rating")]
+        public int AgeRating { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]  
+        public decimal Price { get; set; }
     }
 }

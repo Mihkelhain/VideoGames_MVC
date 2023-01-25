a9f11b276ca10345a396a5f20199cbb66ef830fb
Mihkel Hain <Mihkelhain7@gmail.com>
2023-01-26 00:48:03 +0200

    Add Validitation

--- VideoGames_MVC/Models/VideoGames.cs
+++ VideoGames_MVC/Models/VideoGames.cs
@@ -9,16 +9,30 @@ namespace VideoGames_MVC.Models
     public class VideoGames
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
+
+        [Range(1, 100)]
         [Display(Name = "Age Rating")]
         public int AgeRating { get; set; }
+
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
-        [Column(TypeName = "decimal(18, 2)")]  
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
         public bool MultiPlayer { get; set; }
     }
 }
